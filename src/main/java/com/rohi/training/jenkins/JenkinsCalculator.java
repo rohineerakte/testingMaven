83860912843e811d65f4cb13cbacfa53d50917e0
rohineerakte <rohinee.rakte@gmail.com>
2024-08-24 18:54:31 +0530

    cromr broser files

--- src/main/java/com/rohi/training/jenkins/JenkinsCalculator.java
+++ src/main/java/com/rohi/training/jenkins/JenkinsCalculator.java
@@ -1,17 +1,21 @@
 package com.rohi.training.jenkins;
 
+
+
 public class JenkinsCalculator {
 	//Method to add 2numbers
 	
-	public int addNumbers(int numberOne, int numberTwo) {
-		return numberOne + numberTwo;
-	}
+//	public int addNumbers(int numberOne, int numberTwo) {
+//		return numberOne + numberTwo;
+//	}
+//	
+//	
+//	//Method to subtract 2 number
+//	public int subtractNumbers(int numberOne, int numberTwo) {
+//		return numberOne - numberTwo;
+//	}
 	
-	
-	//Method to subtract 2 number
-	public int subtractNumbers(int numberOne, int numberTwo) {
-		return numberOne - numberTwo;
-	}
 	
 
 }
+ 
--- src/test/java/com/rohi/training/jenkins/JenkinsCalcTest.java
+++ src/test/java/com/rohi/training/jenkins/JenkinsCalcTest.java
@@ -1,20 +1,26 @@
 package com.rohi.training.jenkins;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JenkinsCalcTest {
 
-	@Test
-	public void addTest() {
-		JenkinsCalculator myCalc= new JenkinsCalculator();
-		assertEquals(10,myCalc.addNumbers(5,5));
+//	@Test
+//	public void addTest() {
+//		JenkinsCalculator myCalc= new JenkinsCalculator();
+//		assertEquals(10,myCalc.addNumbers(5,5));
+//	}
+//	@Test
+//	public void subtractTest() {
+//		JenkinsCalculator myCalc= new JenkinsCalculator();
+//		assertEquals(5,myCalc.subtractNumbers(10,5));
+//	}
+	
+	public static void main(String[] args) {
+		WebDriver driver =  new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.close();
 	}
-	@Test
-	public void subtractTest() {
-		JenkinsCalculator myCalc= new JenkinsCalculator();
-		assertEquals(5,myCalc.subtractNumbers(10,5));
-	}
+
 
 }
