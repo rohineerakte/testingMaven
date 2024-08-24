f11a451745b8ac92d0bd554d4fa6ea17cea893cb
rohineerakte <rohinee.rakte@gmail.com>
2024-08-24 17:55:52 +0530

    first commit

---
+++ src/main/java/com/rohi/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,17 @@
+package com.rohi.training.jenkins;
+
+public class JenkinsCalculator {
+	//Method to add 2numbers
+	
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	
+	//Method to subtract 2 number
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+	
+
+}
---
+++ src/test/java/com/rohi/training/jenkins/JenkinsCalcTest.java
@@ -0,0 +1,20 @@
+package com.rohi.training.jenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JenkinsCalcTest {
+
+	@Test
+	public void addTest() {
+		JenkinsCalculator myCalc= new JenkinsCalculator();
+		assertEquals(10,myCalc.addNumbers(5,5));
+	}
+	@Test
+	public void subtractTest() {
+		JenkinsCalculator myCalc= new JenkinsCalculator();
+		assertEquals(5,myCalc.subtractNumbers(10,5));
+	}
+
+}
